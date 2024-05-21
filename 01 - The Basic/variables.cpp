24ec2a939576de27268a2f263e0e621504dd0e19
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-05-21 06:44:51 -0300

    Update variables.cpp

--- 01 - The Basic/variables.cpp
+++ 01 - The Basic/variables.cpp
@@ -22,6 +22,42 @@
  
  the first one CANT change
  the second one CAN change
+
+ DIFFERENT FORMS OF INITIALIZATION:
+ int a;         // DEFAULT
+ int b = 5;     // INTIAIL VALUE OF B WOULD BE 5
+ int c( 6 );    // INITIAL VALUE DIRECTLY IN PARHENTESIS
+ int d { 7 };   // INITIAL VALUE DIRECTLY IN BRACES
+ int e = { 8 }; // INTIAL VALUE WITH EMPTY BRACES AFTER EQUAL SIGN
+ int f {};      // INITIAL VALUE WITH EMPTY BRACES
+
+ We can initialize variables by the next way:
+
+ int a = (15);
+
+ int num1 = (15), num2 = (15); / Correct Way
+ int num1, num2 = (15); / Incorrect Way
+
+ We can do it with the braces too
+
+ int num1 = {15}, num2 = {15}; 
+
+ EXAMPLE:
+ int num1 {0};        // INITIALIZING WITH THE NUMBER 0
+ std::cout << num1;   // THE COUT WILL BE THE 0 INITIALIZED
+
+ int num1;            // INITIALIZING BY THE DEFAULT
+ std::cout << num1;   // THE COUT WILL BE 0 BY DEFAULT
+
+
+ [[maybe_unused]] ATTRIBUTE:
+ 
+ The attribute [[maybe_unused]] is to specify that an variable, function, class or something else can not be used.
+
+ EXAMPLE:
+
+ [[maybe_unused]] double gravity { 9.8 }; // Here we are saying that this variable CAN NOT be used in the program.
+
 */
 
 #include <iostream>
@@ -51,6 +87,16 @@ int main() {
     int x, y, z;
     x = y = z = 50;
 
+    // Example 3
+    int a;         
+    int b = 5;     
+    int c( 6 );   
+    int d { 7 };   
+    int e = { 8 }; 
+    int f {};      
+
+    // Example 4
+    [[maybe_unused]] double gravity { 9.8 };
     /*
     // This will work
     const int minutesPerHour = 60;
