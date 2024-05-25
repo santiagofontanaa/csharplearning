7be164f378615cec655d3b90bad397482b7936bf
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-05-25 11:33:16 -0300

    Update helloworld.cpp

--- 01 - The Basic/00 - Introduction/helloworld.cpp
+++ 01 - The Basic/00 - Introduction/helloworld.cpp
@@ -1,1 +1,11 @@
+// We call the header file library
+#include <iostream>
 
+// We call the main Function and the things inside the {} will be executed by the name of the Main() function
+int main() {
+    // With cout << we call that that line will print something
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This is my first program in CPP" << std::endl;
+    // End of the function
+    return 0;
+}
