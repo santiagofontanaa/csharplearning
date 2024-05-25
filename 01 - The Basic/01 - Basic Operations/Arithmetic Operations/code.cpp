27b10540db1dfecb3e4bfcf2f81d462de58c59e6
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-05-25 10:09:11 -0300

    Update code.cpp

--- 01 - The Basic/01 - Basic Operations/Arithmetic Operations/code.cpp
+++ 01 - The Basic/01 - Basic Operations/Arithmetic Operations/code.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int main() {
+    // SETTING UP THE VARIABLES
+    int num1 = 10;
+    int num2 = 15;
+
+    int addition = num1 + num2; // Output: 25
+    int substraction = num1 - num2;  // Output: -5
+    int multiplication = num1 * num2; // Output: 150
+    int division = num1 / num2; // Outpu: 0.66
+    int modulus = num1 % num2; // Output: 0
+    int increment = ++num1; // Output: 11
+    int increment2 = num1++; // Output: 10
+    int decrement = --num1; // Output: 9
+    int decrement2 = num1--; // Output: 10
+}
