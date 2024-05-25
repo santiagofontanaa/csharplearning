54c7aea09087a5cb91700c689563bcb679bfd3a1
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-05-25 10:58:45 -0300

    Update code.cpp

--- 01 - The Basic/01 - Basic Operations/Loops/code.cpp
+++ 01 - The Basic/01 - Basic Operations/Loops/code.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int main() {
+    int num1 = 10;
+    int num2 = 15;
+    // FOR LOOP
+    for(num1; num1 <= num2; num1++) {
+        std::cout << num1 << std::endl;
+    }
+
+    // WHILE LOOP
+    while(num1 <= num2) {
+        std::cout << num1 << std::endl;
+        num1++;
+    }
+
+}
